aabef8a955c559cb339b8204281660027f2cd22c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-09 17:08:56 +0000

    mpremote 1.26.0

--- Formula/m/mpremote.rb
+++ Formula/m/mpremote.rb
@@ -3,8 +3,8 @@ class Mpremote < Formula
 
   desc "Tool for interacting remotely with MicroPython devices"
   homepage "https://docs.micropython.org/en/latest/reference/mpremote.html"
-  url "https://files.pythonhosted.org/packages/86/1d/4a194eb385133349954cbf269e673e59e28b9510c7805e955da1cd32f4c6/mpremote-1.25.0.tar.gz"
-  sha256 "d0dcd8ab364d87270e1766308882e536e541052efd64aadaac83bc7ebbea2815"
+  url "https://files.pythonhosted.org/packages/a1/f4/b63592bad49d61f0e79ca58d151eb914f1a3f716e606f436352ee5a1ff94/mpremote-1.26.0.tar.gz"
+  sha256 "7f347318fb6d3bb8f89401d399a05efba39b51c74f747cebe92d3c6a9a4ee0b4"
   license "MIT"
 
   bottle do
@@ -12,6 +12,11 @@ class Mpremote < Formula
   end
 
   depends_on "python@3.13"
+
+  resource "platformdirs" do
+    url "https://files.pythonhosted.org/packages/fe/8b/3c73abc9c759ecd3f1f7ceff6685840859e8070c4d947c93fae71f6a0bf2/platformdirs-4.3.8.tar.gz"
+    sha256 "3d512d96e16bcb959a814c9f348431070822a6496326a4be0911c40b5a74c2bc"
+  end
 
   resource "pyserial" do
     url "https://files.pythonhosted.org/packages/1e/7d/ae3f0a63f41e4d2f6cb66a5b57197850f919f59e558159a4dd3a818f5082/pyserial-3.5.tar.gz"
